a6a54bee7e2a08f25961f7172ecec85b03a945a9
Kamil Grundas <kamil.grundas@gmail.com>
2023-02-26 18:48:30 +0100

    Added first features

--- dungeons.py
+++ dungeons.py
@@ -1,7 +1,7 @@
 import sys
 import pygame
 from settings import Settings
-
+from player import Player
 
 
 class Dungeons():
@@ -11,21 +11,69 @@ class Dungeons():
         pygame.init()
 
         self.settings = Settings()
-        self.screen = pygame.display.set_mode((
-            self.settings.screen_width,
-            self.settings.screen_height))
-
+        self.screen = pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
+        self.settings.screen_width = self.screen.get_rect().width
+        self.settings.screen_height = self.screen.get_rect().height
 
         pygame.display.set_caption("Dungeons")
 
 
+        self.player = Player(self)
+        self.bullets = pygame.sprite.Group()
+        self.aliens = pygame.sprite.Group()
+
+
     def run_game(self):
         while True:
+            self._check_events()
+            self.player.update()
+            self._update_screen()
+
+    def _check_events(self):
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     sys.exit()
 
-            pygame.display.flip()
+                elif event.type == pygame.KEYDOWN:
+                    self._check_keydown_events(event)
+                elif event.type == pygame.KEYUP:
+                    self._check_keyup_events(event)
+
+
+    def _check_keydown_events(self, event):
+
+        if event.key == pygame.K_d:
+            self.player.moving_right = True
+        elif event.key == pygame.K_a:
+            self.player.moving_left = True
+        elif event.key == pygame.K_w:
+            self.player.moving_up = True
+        elif event.key == pygame.K_s:
+            self.player.moving_down = True
+
+        elif event.key == pygame.K_q:
+            sys.exit()
+
+    def _check_keyup_events(self, event):
+
+        if event.key == pygame.K_d:
+            self.player.moving_right = False
+        elif event.key == pygame.K_a:
+            self.player.moving_left = False
+        elif event.key == pygame.K_w:
+            self.player.moving_up = False
+        elif event.key == pygame.K_s:
+            self.player.moving_down = False
+    def _update_screen(self):
+        self.screen.fill(self.settings.bg_color)
+        self.player.blitme()
+
+
+
+
+
+        pygame.display.flip()
+
 
 
 if __name__ == '__main__':
--- settings.py
+++ settings.py
@@ -7,3 +7,7 @@ class Settings:
         self.screen_width = 1200
         self.screen_height = 800
         self.bg_color = (230, 230, 230)
+
+        #Ship settings
+        self.player_speed = 0.5
+        self.player_limit = 3
