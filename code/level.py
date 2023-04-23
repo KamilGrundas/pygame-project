5cae3ca079c8ddea84183b3e3319bb796cd3cde2
Kamil Grundas <kamil.grundas@gmail.com>
2023-04-23 22:15:44 +0200

    Added camera

--- code/level.py
+++ code/level.py
@@ -21,12 +21,14 @@ class Level:
 		self.setup()
 
 	def setup(self):
+		self.player = Player((640,360), self.all_sprites)
 		Generic(
 			pos = (0,0),
 			surf = pygame.image.load("graphics/world/ground.png").convert_alpha(),
-			groups= self.all_sprites
+			groups= self.all_sprites,
+			z = LAYERS["ground"]
 		)
-		self.player = Player((640,360), self.all_sprites)
+
 
 	def draw_bullet(self):
 
@@ -44,7 +46,7 @@ class Level:
 	def run(self,dt):
 		self.display_surface.fill('black')
 		# self.all_sprites.draw(self.display_surface)
-		self.all_sprites.custom_draw()
+		self.all_sprites.custom_draw(self.player)
 		self.all_sprites.update(dt)
 		self.bullets.draw(self.display_surface)
 		self.bullets.update(dt)
@@ -57,10 +59,18 @@ class CameraGroup(pygame.sprite.Group):
 	def __init__(self):
 		super().__init__()
 		self.display_surface = pygame.display.get_surface()
+		self.offset = pygame.math.Vector2()
 
-	def custom_draw(self):
-		for sprite in self.sprites():
-			self.display_surface.blit(sprite.image, sprite.rect)
+	def custom_draw(self, player):
+		self.offset.x = player.rect.centerx - SCREEN_WIDTH / 2
+		self.offset.y = player.rect.centery - SCREEN_HEIGHT / 2
+
+		for layer in LAYERS.values():
+			for sprite in self.sprites():
+				if sprite.z == layer:
+					offset_rect = sprite.rect.copy()
+					offset_rect.center -= self.offset
+					self.display_surface.blit(sprite.image, offset_rect)
 
 		
 
